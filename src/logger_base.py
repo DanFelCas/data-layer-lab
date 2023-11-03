91c089468b1f81ea03dc9959b3822a432a7592c3
Daniel Felipe Castro <daniel.castro1024@outlook.com>
2023-11-02 20:10:25 -0500

    logger configuration

---
+++ src/logger_base.py
@@ -0,0 +1,18 @@
+import logging as log
+
+log.basicConfig(
+    level=log.DEBUG,
+    format='[%(asctime)s] %(levelname)s: %(message)s',
+    datefmt='%H:%H:%S',
+    handlers=[
+        log.FileHandler('data_layer.log'),
+        log.StreamHandler()
+    ]
+)
+
+if __name__ == "__main__":
+    log.debug('debug')
+    log.info('info')
+    log.warning('warning')
+    log.error('error')
+    log.critical('critico')
